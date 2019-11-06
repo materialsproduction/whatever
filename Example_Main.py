8d57dfa86b12e9354c8687610b8864a26b7c15dd
Patrick Valdivia <pvaldivia@howardcc.edu>
2019-11-06 13:57:23 -0500

    Hello there, this is commit message

--- Example_Main.py
+++ Example_Main.py
@@ -9,6 +9,29 @@ def getInput():
        nums[i] = input("Please enter a number:")
     return nums
 
+'''
+Created by Cameron Cunningham
+for use during the HCC Git Demo
+'''
+def add(nums):
+    return nums[0] + nums[1]
+def subtract(nums):
+    if nums[0] > nums[1]:
+        return nums[0] - nums[1]
+    else:
+        return nums[1]-nums[0]
+def multiply(nums):
+    return nums[0] * nums[1]
+def divide(nums):
+    return nums[0] / nums[1]
+
+
+
 def main():
     nums = getInput()
+    print(add(nums))
+    print(subtract(nums))
+    print(multiply(nums))
+    print(divide(nums))
+    
 main()
